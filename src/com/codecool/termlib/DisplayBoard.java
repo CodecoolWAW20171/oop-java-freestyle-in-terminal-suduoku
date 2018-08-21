abbe5e730635f719771bea7ca26c115ef5d9a298
WeronikaDebowska <weronika.debowska@gmail.com>
2018-08-21 22:42:40 +0200

    simple table displayed

--- src/com/codecool/termlib/DisplayBoard.java
+++ src/com/codecool/termlib/DisplayBoard.java
@@ -3,14 +3,42 @@ import java.util.Arrays;
 
 public class DisplayBoard {
 
+    static String upperFrame = "┌---┬---┬---┬---┬---┬---┬---┬---┬---┐";
+    static String verticalLine = " │ ";
+    static String middleFrame = "├---┼---┼---┼---┼---┼---┼---┼---┼---┤";
+    static String downFrame= "└---┴---┴---┴---┴---┴---┴---┴---┴---┘";
+    static String[][] boardToDisplay = new String[9][9];
+
     public static void displayGameBoard(Field[][] board){
+        String[][] boardToDisplay = new String[9][9];
         for (int i =0; i < 9; i++) {
             for (int j = 0; j < 9; j++) {
-                System.out.print(board[i][j].getUserValue());
-                System.out.print(" ");
+                if (board[i][j].getUserValue() == 0) {
+                    boardToDisplay[i][j] = " ";
+                } else {
+                    boardToDisplay[i][j] = Integer.toString(board[i][j].getUserValue());
+                }
+            }
+        }
+
+        System.out.println(upperFrame);
+        for (int i =0; i < 8; i++) {
+            System.out.print("│ ");
+            for (int j = 0; j < 9; j++) {
+                System.out.print(boardToDisplay[i][j]);
+                System.out.print(verticalLine);
             }
             System.out.println(" ");
+            System.out.println(middleFrame);
         }
+        int i = 8;
+        System.out.print("│ ");
+        for (int j = 0; j < 9; j++) {
+            System.out.print(boardToDisplay[i][j]);
+            System.out.print(verticalLine);
+        }
+        System.out.println(" ");
+        System.out.println(downFrame);
 
     }
 }
