3b478bbf868eb8422509f0b261096f05d999636d
Tomasz Fijalkowski <tomelecf@gmail.com>
2018-08-21 22:37:33 +0200

    Added checkBoard method

---
+++ src/com/codecool/termlib/Game.java
@@ -0,0 +1,20 @@
+package com.codecool.termlib;
+
+import com.codecool.termlib.Field;
+
+public class Game {
+
+
+    public static boolean checkBoard(Field[][] board){
+        boolean gameWon = true;
+        for (int row = 0; row < 9; row++) {
+            for (int column = 0; column < 9; column++) {
+                if (board[row][column].getUserValue() != board[row][column].getCorrectValue()){
+                    gameWon = false;
+                    break;
+                }
+            }
+        }
+        return gameWon;
+    }
+}
