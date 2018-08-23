5c41a040434918cf2083f5da7dc2a6de5706c80c
Tomasz Fijalkowski <tomelecf@gmail.com>
2018-08-23 12:30:11 +0200

    Added giveAHint method

--- src/com/codecool/termlib/Controls.java
+++ src/com/codecool/termlib/Controls.java
@@ -14,7 +14,7 @@ public class Controls {
 
     public static char takeInput(Field[][] board){
         Scanner scanner = new Scanner(System.in);
-        char[] numbers = new char[] {'1','2','3','4','5','6','7','8','9'};
+        char[] numbers = new char[] {'0','1','2','3','4','5','6','7','8','9'};
         char[] wsad = new char[] {'w','s','a','d','W','S','A','D'};
         char inputChar = 'x';
         String input;
